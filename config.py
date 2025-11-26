4de21cfc79dc5ed497e8c7907243a140b3aaf89d
Nardos Tatek <narditatek@gmail.com>
2025-11-26 09:43:38 +0100

    use separate iterations for the training and infernce step, change the activation fun to identity,

--- config.py
+++ config.py
@@ -1,23 +1,24 @@
 class Config:
     SEED = 42
-    n_embed = 32
-    seq_len = 12
+    n_embed = 12
+    seq_len =  12
     batch_size = 5
     vocab_size = 11710 # data vocab size + special tokens = 11706 + 4
     n_heads = 2
-    n_layers = 6
-    dropout_rate = 0.1
-    eta = 0.0001
+    n_layers = 4
+    dropout_rate = 0.0
+    eta = 0.00001
     exp_dir = "exp" 
     pos_learnable = True
     optim_type = "adam"
-    num_iter = 20
-    wub = 0.01
-    wlb = -0.01
-    tau_m = 10.
-    act_fx = "tanh"
+    num_iter = 2
+    n_iter= 20
+    wub = 0.02
+    wlb = -0.02
+    tau_m = 5.
+    act_fx = "identity"
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
-    tokenizer = "tiktoken"
+    tokenizer = "BPE"
     # When tokenizer == "tiktoken", tokenizer_name is used (e.g. "gpt2" or "cl100k_base")
     tokenizer_name = "gpt2"
 

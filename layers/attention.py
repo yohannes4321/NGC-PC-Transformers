5ca400aa803656a7748139771ccdd1e95b018830
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-12-31 17:53:25 +0300

    change prior

--- layers/attention.py
+++ layers/attention.py
@@ -38,17 +38,17 @@ class Attention:
         self.W_q = HebbianSynapse(f"{prefix}W_q", shape=(n_embed, n_embed), batch_size=batch_size * seq_len, eta=eta,
                                 weight_init=dist.uniform(amin=wlb, amax=wub),
                                 bias_init=dist.constant(value=0.), w_bound=0., 
-                                optim_type=optim_type, sign_value= -1.0, key=subkeys[0],prior=("l1l2", 0.001))
+                                optim_type=optim_type, sign_value= -1.0, key=subkeys[0],prior=("l1l2", (0.001, 0.001)))
         
         self.W_k = HebbianSynapse(f"{prefix}W_k", shape=(n_embed, n_embed), batch_size=batch_size * seq_len, eta=eta,
                                 weight_init=dist.uniform(amin=wlb, amax=wub),
                                 bias_init=dist.constant(value=0.), w_bound=0., 
-                                optim_type=optim_type, sign_value= -1.0, key=subkeys[1],prior=("l1l2", 0.001))
+                                optim_type=optim_type, sign_value= -1.0, key=subkeys[1],prior=("l1l2", (0.001, 0.001)))
         
         self.W_v = HebbianSynapse(f"{prefix}W_v", shape=(n_embed, n_embed), batch_size=batch_size * seq_len, eta=eta,
                                 weight_init=dist.uniform(amin=wlb, amax=wub),
                                 bias_init=dist.constant(value=0.), w_bound=0., 
-                                optim_type=optim_type, sign_value= -1.0, key=subkeys[2],prior=("l1l2", 0.001))
+                                optim_type=optim_type, sign_value= -1.0, key=subkeys[2],prior=("l1l2", (0.001, 0.001)))
        
         self.attn_block = AttentionBlock(f"{prefix}attn_block", n_heads=n_heads, 
                                        n_embed=n_embed, seq_len=seq_len,

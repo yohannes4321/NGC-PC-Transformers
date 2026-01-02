da4c6e654e358c8fbe9c7a4ebcd4a2fffaea8193
Bethlehem <93065948+Betty987@users.noreply.github.com>
2026-01-02 11:40:15 +0300

    change the act_fx to identity

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 class Config:
     SEED = 42
-
     seq_len =  12
     n_embed = 12
-    
-    batch_size = 8
+    batch_size = 16
     vocab_size = 11710# data vocab size + special tokens = 11706 + 4
     n_heads = 2
     n_layers = 4
@@ -13,13 +11,13 @@ class Config:
     exp_dir = "exp" 
     pos_learnable = True
     optim_type = "adam"
-    num_iter = 3
-    n_iter= 20
+    num_iter = 1
+    n_iter= 1
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04
     wub = 0.05
     wlb = -0.05
     tau_m = 10.
-    act_fx = "tanh"
+    act_fx = "identity"
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
     tokenizer = "BPE"
     # When tokenizer == "tiktoken", tokenizer_name is used (e.g. "gpt2" or "cl100k_base")

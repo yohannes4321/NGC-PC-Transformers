1d01d39fd502d0adab7d22d7f3ae119a7924ea07
Nardos Tatek <narditatek@gmail.com>
2026-01-14 08:35:55 +0100

    use the latest tuning values

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 class Config:
     SEED = 42
-    seq_len =  9
+    seq_len =  8
     n_embed = 16
     batch_size = 2
     vocab_size = 11710# data vocab size + special tokens = 11706 + 4
     n_heads = 2
-    n_layers = 2
+    n_layers = 8
     dropout_rate = 0.0
-    eta = 4.919042890915579e-06
+    eta = 1.124861336672477e-06
     exp_dir = "exp" 
     pos_learnable = True
-    optim_type = "sgd"
+    optim_type = "adam"
     epoch = 1
-    n_iter= 26
+    n_iter= 17
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04
-    wub = 0.035284728580901155
-    wlb = -0.07318664527441558
-    tau_m = 11.
+    wub = 0.052687144037646905
+    wlb = -0.09881737158539727
+    tau_m = 17.
     act_fx = "identity"
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
     tokenizer = "BPE"

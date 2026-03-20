5c41172c5a6160261cd05c7127729abf1a102a60
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-03-20 08:09:58 +0300

    break eval in 20 batch

--- eval.py
+++ eval.py
@@ -19,30 +19,26 @@ def eval_model(model: NGCTransformer, data_loader, vocab_size: int):
     total_tokens = 0
     batch_idx = 0
 
+    max_batches = 20
     for batch in data_loader:
-        inputs = batch[0][1]         
-        targets = batch[1][1]        
-
-       
+        if batch_idx >= max_batches:
+            break
+        inputs = batch[0][1]
+        targets = batch[1][1]
         targets_flat = jax.nn.one_hot(targets.flatten(), vocab_size)
         yMu_inf, y_mu, _ = model.process(obs=inputs,
                                       lab=targets_flat,
                                       adapt_synapses=False)
-
-        y_pred = y_mu.reshape(-1, vocab_size)   
-
+        y_pred = y_mu.reshape(-1, vocab_size)
         total_nll += measure_CatNLL(y_pred, targets_flat) * targets_flat.shape[0]
         total_tokens += targets_flat.shape[0]
-        
         if batch_idx % 10 == 0:
             y_pred = y_mu.reshape(-1, vocab_size)
             y_true = targets_flat
-            
             batch_nll = measure_CatNLL(y_pred, y_true)
-            batch_ce_loss = batch_nll.mean()  
+            batch_ce_loss = batch_nll.mean()
             batch_ppl = jnp.exp(batch_ce_loss)
             print(f" Eval Batch {batch_idx}: | CE = {batch_ce_loss:.4f} | PPL = {batch_ppl:.4f}")
-
         batch_idx += 1
 
     ce = total_nll / total_tokens

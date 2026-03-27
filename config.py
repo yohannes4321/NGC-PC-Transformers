426796e57ab32039003d005d1fcaab1bba4b9fec
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-03-27 15:47:17 +0300

    change the config

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 class Config:
     SEED = 42
-    seq_len =  9
+    seq_len = 8
     n_embed = 16
     batch_size = 2
-    vocab_size = 11710# data vocab size + special tokens = 11706 + 4
+    vocab_size = 11710 # data vocab size + special tokens = 11706 + 4
     n_heads = 2
-    n_layers = 2
+    n_layers = 8
     dropout_rate = 0.0
-    eta = 4.919042890915579e-06
-    exp_dir = "exp" 
+    eta = 1.0251644347726223e-06
+    exp_dir = "exp"
     pos_learnable = True
-    optim_type = "sgd"
+    optim_type = "adam"
     epoch = 1
-    n_iter= 26
+    n_iter = 1
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04
-    wub = 0.035284728580901155
-    wlb =  -0.07318664527441558
-    tau_m = 11.
-    act_fx = "identity"
+    wub = 0.06036543037871054
+    wlb = -0.0988138707057169
+    tau_m = 13.
+    act_fx = "relu"
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
     tokenizer = "BPE"
     # When tokenizer == "tiktoken", tokenizer_name is used (e.g. "gpt2" or "cl100k_base")

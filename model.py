eea10b6a6fa24064a0a5c1d3838479a6217e347f
Yohannes Alemu <alemuyohannes960@gmail.com>
2026-04-08 17:39:08 +0300

    feat: include L4 in EFE calculation within model architecture

--- model.py
+++ model.py
@@ -578,7 +578,7 @@ class NGCTransformer:
                 block = self.blocks[i]
                 block_errors += block.attention.e_qkv.L.get() + block.attention.e_attn.L.get() + block.mlp.e_mlp2.L.get() + block.mlp.e_mlp1.L.get()
 
-        EFE =  block_errors + L1
+        EFE =  block_errors + L1 + L4   
 
         if adapt_synapses == True:
                 self.embedding_evolve.run()

a65e4738c4eac4decf0d195464b087ab868b8f5d
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-14 19:20:50 +0300

    refactor: remove unnecessary print statement in NGCTransformer class

--- model.py
+++ model.py
@@ -651,7 +651,7 @@ class NGCTransformer:
                 block_errors += block.attention.e_attn.L.get() + block.mlp.e_mlp.L.get() + block.mlp.e_mlp1.L.get()
 
         EFE = L4 + block_errors + L1
-        print("L")
+        
 
         if adapt_synapses == True:
                 self.embedding_evolve.run()

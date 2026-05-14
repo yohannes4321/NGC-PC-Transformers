587ef6761cea60913539cce5143b2f2d0ec7898d
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-14 14:37:34 +0300

    fix: update learning rate (eta) in Config class to 1e-3

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     n_heads = 2
     n_layers = 2
     dropout_rate = 0.0
-    eta = 4.919042890915579e-06
+    eta = 1e-3
     exp_dir = "exp" 
     pos_learnable = True
     optim_type = "sgd"

93929b0606b329cde62aa7d24280e2f5c97374f0
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-20 15:04:00 +0300

    fix output layer sign_value in HebbianSynapse initialization for correct weight updates

--- layers/output.py
+++ layers/output.py
@@ -29,7 +29,7 @@ class Output:
         
         self.W_out = HebbianSynapse(
                     "W_out", shape=(n_embed, vocab_size), batch_size= batch_size * seq_len, eta=config.eta_o, weight_init=dist.gaussian(mean=0.0, std=0.005),
-                    bias_init=dist.constant(value=0.), w_bound=1., optim_type="adam", sign_value= 1.0, key=subkeys[4],prior=("constant", 0.))
+                    bias_init=dist.constant(value=0.), w_bound=1., optim_type="adam", sign_value= -1.0, key=subkeys[4],prior=("constant", 0.))
         self.e_out = ErrorCell("e_out", n_units=vocab_size, 
                                   batch_size=batch_size * seq_len) # shape=(seq_len, vocab_size, 1),
         self.E_out = StaticSynapse(

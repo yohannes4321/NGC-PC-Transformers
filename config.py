5a89c6b1fba28103d9fdcfd07aaf5bb8786e1461
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-20 16:50:28 +0300

    refactor: update learning rate and optimization type, enhance weight bounds in HebbianSynapse layers

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class Config:
     n_heads = 2
     n_layers = 2
     dropout_rate = 0.0
-    eta = 1e-3
+    eta = 1e-4
     exp_dir = "exp" 
     pos_learnable = True
-    optim_type = "sgd"
+    optim_type = "adam"
     epoch = 1
     n_iter= 26
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04

6e5a7cd064756eab3b00031d7884b8eed099e3d5
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-21 21:18:06 +0300

    refactor Config class: restore hyperparameters to original values for consistency and performance

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
 class Config:
     SEED = 42
-    seq_len = 25
-    n_embed = 72
-    batch_size = 6
+    seq_len = 22
+    n_embed = 64
+    batch_size = 3
     vocab_size = 11710# data vocab size + special tokens = 11706 + 4
-    n_heads = 3
+    n_heads = 2
     n_layers = 3
-    dropout_rate = 0.08338083927952628
-    eta = 2.8302987877207265e-06
+    dropout_rate = 0.059089954105342984
+    eta = 1.1696389681412456e-06
     eta_o= 1.0e-03
     exp_dir = "exp" 
     pos_learnable = True
     optim_type = "adam"
     epoch = 1
-    n_iter= 4
+    n_iter= 9
     tau_o = 2
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04
-    wub = 0.016874106399569447
-    wlb = -0.017259679300295518
+    wub = 0.029535766439250218
+    wlb = -0.029160352876638193
     wu = 0.025
     wl = -0.025
-    tau_m = 34.0
-    act_fx = "tanh"
+    tau_m = 31.0
+    act_fx = "relu"
     act_fx_o = "identity"
-    embed_mult = 24
+    embed_mult = 32
 
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
     tokenizer = "BPE"

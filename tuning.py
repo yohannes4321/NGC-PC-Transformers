ffb368fcb30fc9ebf8a4b6e10bf440ffccb07bab
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-23 18:24:13 +0300

    fix: increase EFE stability threshold for improved model robustness

--- tuning.py
+++ tuning.py
@@ -24,7 +24,7 @@ from eval import eval_model
 from config import Config as base_config
 from ngclearn.utils.metric_utils import measure_CatNLL
 
-EFE_STABILITY_THRESHOLD = 1e3
+EFE_STABILITY_THRESHOLD = 1e4
 
 
 def define_search_space(trial):

b825b0e094347f292dd3fcbcce883e347f91cba1
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-29 19:23:33 +0300

    refactor config: update model parameters for improved performance

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     SEED = 42
-    seq_len =32
-    n_embed = 64
-    batch_size = 16
+    seq_len =64
+    n_embed = 96
+    batch_size = 12
     vocab_size = 11710# data vocab size + special tokens = 11706 + 4
     n_heads = 8
     n_layers = 2
@@ -11,9 +11,9 @@ class Config:
     eta_o= 2.9e-03
     exp_dir = "exp" 
     pos_learnable = True
-    optim_type = "adam"
+    optim_type = "sgd"
     epoch = 1
-    n_iter= 20
+    n_iter= 26
     tau_o = 2
     # Approximate Xavier scaling: 1 / sqrt(512) is about 0.04
     wub = 0.035284728580901155
@@ -21,7 +21,7 @@ class Config:
     wu = 0.035284728580901155
     wl = -0.035284728580901155
     tau_m = 2.7
-    act_fx = "relu"
+    act_fx = "identity"
     act_fx_o = "identity"
 
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"

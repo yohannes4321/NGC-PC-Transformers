e97f78f0ccca7217620c2244616588382ef42287
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-30 14:28:11 +0300

    comment out reset processes in NGCTransformer class

--- model.py
+++ model.py
@@ -315,12 +315,12 @@ class NGCTransformer:
                    
                    
                    
-                    reset_process >> block.attention.z_qkv.reset
-                    reset_process >> block.attention.z_attn.reset
+                    # reset_process >> block.attention.z_qkv.reset
+                    # reset_process >> block.attention.z_attn.reset
                     reset_process >> block.attention.e_qkv.reset
                     reset_process >> block.attention.e_attn.reset
-                    reset_process >> block.mlp.z_mlp.reset
-                    reset_process >> block.mlp.z_mlp2.reset
+                    # reset_process >> block.mlp.z_mlp.reset
+                    # reset_process >> block.mlp.z_mlp2.reset
                     reset_process >> block.mlp.e_mlp.reset
                     reset_process >> block.mlp.e_mlp1.reset
                     reset_process >> block.reshape_3d_to_2d.reset
@@ -349,10 +349,10 @@ class NGCTransformer:
                 reset_process >> self.projection.q_out_Ratecell.reset
                 reset_process >> self.projection.q_target_Ratecell.reset
                 reset_process >> self.projection.eq_target.reset
-                reset_process >> self.embedding.z_embed.reset
-                reset_process >> self.output.z_out.reset
-                reset_process >> self.z_target.reset
-                reset_process >> self.z_actfx.reset
+                # reset_process >> self.embedding.z_embed.reset
+                # reset_process >> self.output.z_out.reset
+                # reset_process >> self.z_target.reset
+                # reset_process >> self.z_actfx.reset
                 reset_process >> self.embedding.e_embed.reset
                 reset_process >> self.output.e_out.reset
                 reset_process >> self.output.W_out.reset

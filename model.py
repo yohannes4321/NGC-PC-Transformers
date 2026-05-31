52ff2fba04a1923f25ba4561c8a6e35b10eb889a
Yohannes Alemu <yohannesalemu960@gmail.com>
2026-05-31 19:04:49 +0300

    j

--- model.py
+++ model.py
@@ -64,46 +64,43 @@ class NGCTransformer:
             makedir(exp_dir + "/filters")
 
         dkey, *subkeys = random.split(dkey, 50)
-       
-        with Context("Circuit") as self.circuit:
-                
-            self.embedding = EMBEDDING(dkey=subkeys[0], vocab_size=self.vocab_size, seq_len=self.seq_len, embed_dim=self.n_embed, batch_size=self.batch_size, pos_learnable=pos_learnable, eta=eta, optim_type=optim_type)
-                
-            self.blocks = []
-            for i in range(n_layers):
-                key, subkey = random.split(subkeys[1 + i])
-                block=Block(dkey=subkey, block_id= i, n_embed=self.n_embed, seq_len=self.seq_len,
-                                batch_size=self.batch_size, vocab_size=self.vocab_size, n_heads=n_heads, dropout_rate=dropout_rate, eta=eta, optim_type=optim_type, wub=wub, wlb=wlb, tau_m=tau_m)
-                self.blocks.append(block)   
-                    
-            self.output = Output(dkey=subkeys[3], n_embed=self.n_embed, seq_len=self.seq_len, batch_size=self.batch_size, vocab_size=self.vocab_size, eta=eta, optim_type=optim_type, wlb=wlb, wub=wub, tau_m=tau_m)
-                
-            self.z_target=RateCell("z_target", n_units= self.vocab_size, tau_m=0., act_fx="identity", batch_size=self.batch_size * self.seq_len) 
-            self.z_actfx= RateCell("z_actfx", n_units= self.vocab_size, tau_m=tau_m, act_fx="softmax", batch_size=self.batch_size * self.seq_len)
-            self.projection = Projection(dkey=subkeys[29], n_embed=self.n_embed, seq_len=self.seq_len, batch_size=self.batch_size,
-                                             vocab_size=self.vocab_size, eta=eta, optim_type=optim_type, pos_learnable=pos_learnable, wub=wub, wlb=wlb, n_blocks=n_layers, n_heads=n_heads, dropout_rate=dropout_rate)
-            self.reshape_4d_to_2d = ReshapeComponent("reshape_4d_to_2d",
-                                            input_shape=(self.batch_size, self.seq_len, self.n_embed, 1),
-                                            output_shape=(self.batch_size * self.seq_len, self.n_embed))
-                
-            self.reshape_3d_to_2d_embed = ReshapeComponent("reshape_3d_to_2d_embed",
-                                            input_shape=(self.batch_size, self.seq_len, self.n_embed),
-                                            output_shape=(self.batch_size * self.seq_len, self.n_embed))
-            self.reshape_2d_to_3d_embed= ReshapeComponent("reshape_2d_to_3d_embed",
-                                            input_shape=(self.batch_size * self.seq_len, self.n_embed),
-                                            output_shape=(self.batch_size, self.seq_len, self.n_embed))
-            self.Outgrad = Outgrad("Outgrad", batch_size=self.batch_size, seq_len=self.seq_len, vocab_size=self.vocab_size)    
-                
         if loadDir is not None:
    
             self.load_from_disk(loadDir,n_layers=n_layers)
           
         else:
             with Context("Circuit") as self.circuit:
-            
+       
+            with Context("Circuit") as self.circuit:
+                    
+                self.embedding = EMBEDDING(dkey=subkeys[0], vocab_size=self.vocab_size, seq_len=self.seq_len, embed_dim=self.n_embed, batch_size=self.batch_size, pos_learnable=pos_learnable, eta=eta, optim_type=optim_type)
+                    
+                self.blocks = []
+                for i in range(n_layers):
+                    key, subkey = random.split(subkeys[1 + i])
+                    block=Block(dkey=subkey, block_id= i, n_embed=self.n_embed, seq_len=self.seq_len,
+                                    batch_size=self.batch_size, vocab_size=self.vocab_size, n_heads=n_heads, dropout_rate=dropout_rate, eta=eta, optim_type=optim_type, wub=wub, wlb=wlb, tau_m=tau_m)
+                    self.blocks.append(block)   
+                        
+                self.output = Output(dkey=subkeys[3], n_embed=self.n_embed, seq_len=self.seq_len, batch_size=self.batch_size, vocab_size=self.vocab_size, eta=eta, optim_type=optim_type, wlb=wlb, wub=wub, tau_m=tau_m)
+                    
+                self.z_target=RateCell("z_target", n_units= self.vocab_size, tau_m=0., act_fx="identity", batch_size=self.batch_size * self.seq_len) 
+                self.z_actfx= RateCell("z_actfx", n_units= self.vocab_size, tau_m=tau_m, act_fx="softmax", batch_size=self.batch_size * self.seq_len)
+                self.projection = Projection(dkey=subkeys[29], n_embed=self.n_embed, seq_len=self.seq_len, batch_size=self.batch_size,
+                                                vocab_size=self.vocab_size, eta=eta, optim_type=optim_type, pos_learnable=pos_learnable, wub=wub, wlb=wlb, n_blocks=n_layers, n_heads=n_heads, dropout_rate=dropout_rate)
                 
-                self.embedding.z_embed.zF   >> self.embedding.W_embed.inputs
-                self.embedding.W_embed.outputs >> self.reshape_3d_to_2d_embed.inputs  
+                self.reshape_3d_to_2d_embed = ReshapeComponent("reshape_3d_to_2d_embed",
+                                                input_shape=(self.batch_size, self.seq_len, self.n_embed),
+                                                output_shape=(self.batch_size * self.seq_len, self.n_embed))
+                self.reshape_2d_to_3d_embed= ReshapeComponent("reshape_2d_to_3d_embed",
+                                                input_shape=(self.batch_size * self.seq_len, self.n_embed),
+                                                output_shape=(self.batch_size, self.seq_len, self.n_embed))
+                self.Outgrad = Outgrad("Outgrad", batch_size=self.batch_size, seq_len=self.seq_len, vocab_size=self.vocab_size)    
+                    
+                # CORRECT PREDICTIVE CODING FLOW
+                self.embedding.z_embed.zF >> self.embedding.W_embed.inputs
+                self.embedding.W_embed.outputs >> self.reshape_3d_to_2d_embed.inputs
+                self.reshape_3d_to_2d_embed.outputs >> self.blocks[0].attention.z_qkv.j
                 self.reshape_3d_to_2d_embed.outputs >> self.embedding.e_embed.mu
                 self.blocks[0].attention.z_qkv.z >> self.embedding.e_embed.target
                 
@@ -325,7 +322,6 @@ class NGCTransformer:
                     reset_process >> block.reshape_2d_to_3d_q.reset
                     reset_process >> block.reshape_2d_to_3d_k.reset
                     reset_process >> block.reshape_2d_to_3d_v.reset
-                    reset_process >> block.reshape_3d_to_2d_attnout.reset
             
                     evolve_process >> block.attention.W_q.evolve
                     evolve_process >> block.attention.W_k.evolve
@@ -506,73 +502,30 @@ class NGCTransformer:
     def process(self, obs, lab, adapt_synapses=True):
         
         self.reset.run()
-        # self.projection.Q_embed.word_weights.set(self.embedding.W_embed.word_weights.get())
-        # if self.embedding.W_embed.pos_learnable:
-        #    self.projection.Q_embed.pos_weights.set(self.embedding.W_embed.pos_weights.get())
-        # for i in range(self.n_layers):
-        #     block_proj= self.projection.blocks[i]
-        #     block= self.blocks[i] 
-        #     block_proj.Q_q.weights.set(block.attention.W_q.weights.get())
-        #     block_proj.Q_q.biases.set(block.attention.W_q.biases.get())
-        #     block_proj.Q_k.weights.set(block.attention.W_k.weights.get())
-        #     block_proj.Q_k.biases.set(block.attention.W_k.biases.get())
-        #     block_proj.Q_v.weights.set(block.attention.W_v.weights.get())
-        #     block_proj.Q_v.biases.set(block.attention.W_v.biases.get())
-        #     block_proj.Q_attn_out.weights.set(block.attention.W_attn_out.weights.get())
-        #     block_proj.q_attn_block.inputs_q.set(block.attention.attn_block.inputs_q.get())
-        #     block_proj.q_attn_block.inputs_k.set(block.attention.attn_block.inputs_k.get())
-        #     block_proj.q_attn_block.inputs_v.set(block.attention.attn_block.inputs_v.get())
-        #     block_proj.Q_attn_out.biases.set(block.attention.W_attn_out.biases.get())
-        #     block_proj.Q_mlp1.weights.set(block.mlp.W_mlp1.weights.get())
-        #     block_proj.Q_mlp1.biases.set(block.mlp.W_mlp1.biases.get())
-        #     block_proj.Q_mlp2.weights.set(block.mlp.W_mlp2.weights.get())
-        #     block_proj.Q_mlp2.biases.set(block.mlp.W_mlp2.biases.get())
-
-        # self.projection.Q_out.weights.set(self.output.W_out.weights.get())
-        # self.projection.Q_out.biases.set(self.output.W_out.biases.get())
-        # self.projection.q_target_Ratecell.j_td.set(jnp.zeros((self.batch_size * self.seq_len, self.vocab_size)))
-        
-       
         self.clamp_input(obs)
-        self.clamp_infer_target(lab)
-        
-        # self.project.run(t=0., dt=1.)
-
-
+        if lab is not None:
+            self.clamp_infer_target(lab)
         for i in range(self.n_layers):
-        #     block_proj= self.projection.blocks[i]   
-            b= self.blocks[i]
-        #     b.attention.z_qkv.z.set(block_proj.q_qkv_Ratecell.z.get())
-        #     b.mlp.z_mlp.z.set(block_proj.q_mlp_Ratecell.z.get())
-        #     b.mlp.z_mlp2.z.set(block_proj.q_mlp2_Ratecell.z.get())
+            b = self.blocks[i]
             b.attention.E_q.weights.set(jnp.transpose(b.attention.W_q.weights.get()))
             b.attention.E_k.weights.set(jnp.transpose(b.attention.W_k.weights.get()))
             b.attention.E_v.weights.set(jnp.transpose(b.attention.W_v.weights.get()))
             b.attention.E_attn.weights.set(jnp.transpose(b.attention.W_attn_out.weights.get()))
             b.mlp.E_mlp.weights.set(jnp.transpose(b.mlp.W_mlp2.weights.get()))  
             b.mlp.E_mlp1.weights.set(jnp.transpose(b.mlp.W_mlp1.weights.get()))
-       
         self.output.E_out.weights.set(jnp.transpose(self.output.W_out.weights.get()))
-        # self.output.z_out.z.set(self.projection.q_out_Ratecell.z.get())
-        # self.output.e_out.dmu.set(self.projection.eq_target.dmu.get())
-        # self.output.e_out.dtarget.set(self.projection.eq_target.dtarget.get())
-        
-        
-        ## get projected prediction (from the P-step)
         y_mu_inf = self.projection.q_target_Ratecell.z.get()
-    
-        EFE = 0. 
-        y_mu = 0.
-        #if adapt_synapses:
         for ts in range(0, self.T):
         
             self.clamp_input(obs)
-            self.clamp_target(lab)
-             
-            self.advance.run(t=ts,dt=1.)
+            if lab is not None:
+                self.clamp_target(lab)
+            self.advance.run(t=ts, dt=1.)
+          
            
-        # y_mu = self.output.W_out.outputs.get() 
-        y_mu = self.z_actfx.zF.get() 
+        # Get output predictions
+
+        y_mu = self.zactfx.zF.get() 
 
         L1 = self.embedding.e_embed.L.get()
         L4 = self.output.e_out.L.get()

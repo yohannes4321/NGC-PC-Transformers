b4be2e3003d44512450272d1323ec4bcaaed85ea
quinbez <bezawitt9@gmail.com>
2026-06-07 00:23:12 +0300

    check generate and vocab size

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class Config:
 
     # Tokenizer selection: "BPE" (custom/BPE loader) or "tiktoken"
     tokenizer = "tiktoken"
-    tokenizer_encoding = "gpt2"
+    tokenizer_encoding = "cl100k_base"
 
     tokenizer_vocab_file = None
 
